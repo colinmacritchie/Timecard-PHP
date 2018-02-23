aebce975de887c26bcc73a9512bc91dbe45a1c01
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2018-02-23 11:25:09 -0700

    Add Get Function

--- timecard.php
+++ timecard.php
@@ -34,3 +34,25 @@ if($_POST) {
         mysql_query("INSERT INTO 'timecards' SET `client`='$client', `punch`='$now', `comment`='$cmt'");
 }
 
+if($_GET) {
+  $start = date('Y-m-d H:i:s', strtotime($_GET['s']));
+  $end = date('Y-m-d H:i:s', strtotime($_GET['e']));
+  $client = preg_replace('/a-zA-Z0-9 ]+/', '', $_GET['client']);
+  $query = mysql_query("SELECT * FROM `timecards` WHERE `client`='$client' && `punch` BETWEEN '$start' AND '$end' ORDER BY `punch`");
+  while($row=mysql_fetch_assoc($query)) {
+    $q = question(base64_decode($row['comment']));
+    if($t===null) {
+      $t = $row['punch'];
+      $results .= "<br />".format($row['punch']).$q;
+    } else {
+        $j = timediff($t, $row['punch']);
+        $results .= "to ".format($row['punch']).$q." = ".His($j);
+        $total += $j;
+        $t = null;
+    }
+  }
+  if($t !== null) {
+      $results .= " You are still clocked in";
+  }
+  $results .= "<br /><br /><strong>Total:</strong>".His($total);
+}
